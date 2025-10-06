b8e98108739a154701a3e71c41dd6aa25179cf37
ChinGuang <ganchinguang0622@gmail.com>
2025-10-06 11:33:54 +0800

    Add display functionality
    Implements the display component and logic for inserting numbers into it.

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ const calculatorMemory = {
     operation: undefined,
     secondNumber: ''
 }
+
+const displayComponent = document.querySelector("#display-board");
+
+function insertNumber(num) {
+    if (calculatorMemory.operation === undefined) {
+        calculatorMemory.firstNumber += num;
+    } else {
+        calculatorMemory.secondNumber += num;
+    }
+    setDisplayNumber();
+}
+
+function setDisplayNumber() {
+    if (calculatorMemory.operation === undefined) {
+        displayComponent.textContent = calculatorMemory.firstNumber;
+    } else {
+        displayComponent.textContent = calculatorMemory.secondNumber;
+    }
+}
+
+const digitButtons = document.querySelectorAll(".digit-button");
+digitButtons.forEach((digitButton) => {
+    digitButton.addEventListener("click", (e) => {
+        const digit = e.target.textContent;
+        insertNumber(+digit);
+    })
+});
